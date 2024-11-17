8ace12fe623e3a930dea2f11d4f7ed161af2bb68
jeya chandran jcs <jeyachandranjcs@gmail.com>
2024-11-18 00:28:40 +0530

    ok

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-import express from "express"
-import dotenv from "dotenv"
-import cors from "cors"
-import mongoose from "mongoose"
-import userRouter from "./routes/userRoutes.js"
-import itemRoutes from "./routes/itemRoutes.js"
-import orderRoutes from "./routes/orderRoutes.js"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import userRouter from "./routes/userRoutes.js";
+import itemRoutes from "./routes/itemRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
-dotenv.config()
-const app=express()
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const port=process.env.Port
-const mongoUrl=process.env.MONGO_URL
+const port = process.env.Port || 5000; // Fallback to 5000 if Port is undefined
+const mongoUrl = process.env.MONGO_URL;
 
-app.get("/",(req,res)=>{
-    res.send("hello world")
-})
+// console.log("MongoDB URL:", mongoUrl);  // Log to verify the value
 
-app.use("/user",userRouter)
-app.use("/item",itemRoutes)
-app.use("/order",orderRoutes)
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.use("/user", userRouter);
+app.use("/item", itemRoutes);
+app.use("/order", orderRoutes);
 
 mongoose.connect(mongoUrl)
-.then(()=>{
-    app.listen(port,()=>{console.log(`server is running on port ${port}`)})
-})
-.catch((err)=>{
-        console.log("error in server connectivity",err)
-    })
-
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Error in server connectivity", err);
+  });
